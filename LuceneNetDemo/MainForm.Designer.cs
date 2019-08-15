0b276f13dcc5d740244fb1f9e9784a6049fdff46
Leon <leon.id@qq.com>
2019-08-15 13:52:51 +0800

    添加项目文件。

---
+++ LuceneNetDemo/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace LuceneNetDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.BackColor = System.Drawing.Color.White;
+            this.InputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.InputTextBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.InputTextBox.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.InputTextBox.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.InputTextBox.Location = new System.Drawing.Point(0, 0);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(702, 39);
+            this.InputTextBox.TabIndex = 0;
+            this.InputTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // OutputListBox
+            // 
+            this.OutputListBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.OutputListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.OutputListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.OutputListBox.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.OutputListBox.ForeColor = System.Drawing.Color.DimGray;
+            this.OutputListBox.FormattingEnabled = true;
+            this.OutputListBox.ItemHeight = 25;
+            this.OutputListBox.Location = new System.Drawing.Point(0, 39);
+            this.OutputListBox.Name = "OutputListBox";
+            this.OutputListBox.Size = new System.Drawing.Size(702, 266);
+            this.OutputListBox.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 305);
+            this.Controls.Add(this.OutputListBox);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "MainForm";
+            this.Text = "Lucene.Net Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.ListBox OutputListBox;
+    }
+}
+
---
+++ LuceneNetDemo/MainForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LuceneNetDemo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
